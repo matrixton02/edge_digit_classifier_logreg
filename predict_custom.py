bc94ba897a49c5b597faff9297789d6b2a512c9b
matrixton02 <yashasvi21022005@gmail.com>
2025-07-13 15:30:40 +0530

    added predict_custom_logic

---
+++ predict_custom.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+from model_utils import predict_multiclass
+
+def preprocess_custom_digit(image_path):
+    img=cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+    img_resized=cv2.resize(img,(8,8),interpolation=cv2.INTER_AREA)
+
+    if(np.mean(img_resized)>127):
+        img_resized=255-img_resized
+    
+    img_rescaled=(img_resized/255.0)*16.0
+    edge_img=cv2.Canny(np.unit8(img_rescaled*(255.0/img_rescaled.max())),30,100)
+    binary_map=(edge_img>0).astype(int)
+
+    return binary_map.flatten(),img_resized
+
+all_theta=np.load("saved/theta_all.npy")
+
+X_custom, original_img = preprocess_custom_digit("data/my_digit.png")
+pred = predict_multiclass(np.array([X_custom]), all_theta)[0]
+
+print("Predicted Digit:", pred)
+
+# Show original image
+import matplotlib.pyplot as plt
+plt.imshow(original_img, cmap='gray')
+plt.title(f"Predicted: {pred}")
+plt.axis('off')
+plt.show()
